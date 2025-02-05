397c66f53cf42d08e1192fd8db03f487d12b2f92
LeRoiFou <lrcompta@gmail.com>
2025-02-05 18:47:11 +0100

    Multipages

---
+++ index015_MultiPages.py
@@ -0,0 +1,14 @@
+"""
+Cours : Streamlit Mini Course - Make Websites With ONLY Python
+Lien : https://www.youtube.com/watch?v=o8p7uQCGD0U
+
+Multi-pages dans une page @ 
+(répertoire pages inséré avec les fichiers py correspondant)
+
+Date : 05/02/25
+"""
+
+import streamlit as st
+
+st.title("Home")
+
